be4c954ee106a5f3c8dbef6850a59f53889b45e2
mp <mp@arista.com>
2020-12-16 20:15:00 +0100

    Multiple pools

--- terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
+++ terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
@@ -3,44 +3,78 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  rg         = var.static_rg_vnet_info["edge1"]["rg"]
+  vnet       = var.static_rg_vnet_info["edge1"]["vnet"]
+  edge1subnets = ["edge1cloudeos1Intf0", "edge1cloudeos1Intf1", "edge1cloudeos1Intf2",
+                  "edge1cloudeos1Intf3", "edge1cloudeos1Intf4", "edge1cloudeos1Intf5",
+                  "edge1cloudeos1Intf6", "edge1cloudeos1Intf7"]
+  edge2subnets = ["edge2cloudeos1Intf0", "edge2cloudeos1Intf1", "edge2cloudeos1Intf2",
+                  "edge2cloudeos1Intf3", "edge2cloudeos1Intf4", "edge2cloudeos1Intf5",
+                  "edge2cloudeos1Intf6", "edge2cloudeos1Intf7"]
+}
+
 data "azurerm_resource_group" "rg" {
-  name       = var.static_rg_vnet_info["edge1"]["rg"]
+  name       = local.rg
 }
 
 data "azurerm_virtual_network" "vnet" {
-  name                = var.static_rg_vnet_info["edge1"]["vnet"]
-  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+  name                = local.vnet
+  resource_group_name = local.rg
 }
 
 data "azurerm_subnet" "subnet" {
   name                 = var.subnet_info["edge1subnet"]["subnet_names"][count.index]
-  virtual_network_name = var.static_rg_vnet_info["edge1"]["vnet"]
-  resource_group_name  = var.static_rg_vnet_info["edge1"]["rg"]
+  virtual_network_name = local.vnet
+  resource_group_name  = local.rg
   count                = length(var.subnet_info["edge1subnet"]["subnet_names"])
 }
 
-data "azurerm_network_interface" "firstif" {
-  name                = "edge1cloudeos1Intf0"
-  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+data "azurerm_network_interface" "edge1ifs" {
+  count                = length(local.edge1subnets)
+  name                = local.edge1subnets[count.index]
+  resource_group_name = local.rg
+}
+
+data "azurerm_network_interface" "edge2ifs" {
+  count                = length(local.edge2subnets)
+  name                = local.edge2subnets[count.index]
+  resource_group_name = local.rg
 }
 
 resource "azurerm_lb" "cloudedgelb" {
   name                = "AMADEUS-TEST"
   location            = data.azurerm_virtual_network.vnet.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = local.rg
   sku                 = "Standard"
   frontend_ip_configuration {
-    name      = "Amadeus-IP1"
-    subnet_id = data.azurerm_subnet.subnet[0].id
+    name      = "Amadeus-Onprem-DC"
+    subnet_id = data.azurerm_subnet.subnet[2].id
   }
   frontend_ip_configuration {
-    name      = "Amadeus-IP2"
-    subnet_id = data.azurerm_subnet.subnet[0].id
+    name      = "Amadeus-Spoke-DC"
+    subnet_id = data.azurerm_subnet.subnet[3].id
+  }
+  frontend_ip_configuration {
+    name      = "Amadeus-Onprem-EXT"
+    subnet_id = data.azurerm_subnet.subnet[4].id
+  }
+  frontend_ip_configuration {
+    name      = "Amadeus-Spoke-EXT"
+    subnet_id = data.azurerm_subnet.subnet[5].id
+  }
+  frontend_ip_configuration {
+    name      = "Amadeus-Onprem-INT"
+    subnet_id = data.azurerm_subnet.subnet[6].id
+  }
+  frontend_ip_configuration {
+    name      = "Amadeus-Spoke-INT"
+    subnet_id = data.azurerm_subnet.subnet[7].id
   }
 }
 
 resource "azurerm_lb_rule" "rule1" {
-  resource_group_name            = var.static_rg_vnet_info["edge1"]["rg"]
+  resource_group_name            = local.rg
   loadbalancer_id                = azurerm_lb.cloudedgelb.id
   name                           = "rule1"
   protocol                       = "All"
@@ -53,21 +87,27 @@ resource "azurerm_lb_rule" "rule1" {
 }
 
 resource "azurerm_lb_probe" "probe" {
-  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+  resource_group_name = local.rg
   loadbalancer_id     = azurerm_lb.cloudedgelb.id
   name                = "ssh-probe"
   port                = 22
   protocol            = "Tcp"
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "intfpoolassociation1" {
-  network_interface_id    = data.azurerm_network_interface.firstif.id
-  ip_configuration_name   = "edge1cloudeos1Intf0"
+resource "azurerm_network_interface_backend_address_pool_association" "intfpool1association1" {
+  network_interface_id    = data.azurerm_network_interface.edge1ifs[2].id
+  ip_configuration_name   = local.edge1subnets[2]
+  backend_address_pool_id = azurerm_lb_backend_address_pool.pool1.id
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "intfpool1association2" {
+  network_interface_id    = data.azurerm_network_interface.edge2ifs[2].id
+  ip_configuration_name   = local.edge2subnets[2]
   backend_address_pool_id = azurerm_lb_backend_address_pool.pool1.id
 }
 
 resource "azurerm_lb_backend_address_pool" "pool1" {
-  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+  resource_group_name = local.rg
   loadbalancer_id     = azurerm_lb.cloudedgelb.id
   name                = "CloudEOSPool1"
 }
--- terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/output.tf
+++ terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/output.tf
@@ -1,39 +1,4 @@
-output "vnet_id" {
-  description = "The id of the  vNet"
-  value       = data.azurerm_virtual_network.vnet.id
-}
-
-output "vnet_name" {
-  description = "The Name of the  vNet"
-  value       = data.azurerm_virtual_network.vnet.name
-}
-
-output "rg_name" {
-  description = "The Name of the  RG"
-  value       = data.azurerm_resource_group.rg.name
-}
-
-output "vnet_location" {
-  description = "The location of the  vNet"
-  value       = data.azurerm_virtual_network.vnet.location
-}
-
-output "rg_location" {
-  description = "The location of the  vNet"
-  value       = data.azurerm_resource_group.rg.location
-}
-
-output "vnet_address_space" {
-  description = "The address space of the  vNet"
-  value       = data.azurerm_virtual_network.vnet.address_space
-}
-
-output "azurerm_subnet" {
-  description = "The address space of the  vNet"
-  value       = data.azurerm_subnet.subnet
-}
-
-output "azurerm_network_interface" {
-  description = "First interface on VM"
-  value       = data.azurerm_network_interface.firstif
+output "lb_info" {
+  description = "LB info"
+  value       = azurerm_lb.cloudedgelb
 }
