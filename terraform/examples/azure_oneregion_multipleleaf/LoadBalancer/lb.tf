05ad54c349b4fe9aaa2984bb737b70991f4df767
mp <mp@arista.com>
2020-12-16 18:28:05 +0100

    Add vars

--- terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
+++ terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
@@ -4,17 +4,17 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "rg" {
-  name       = var.rg_name
+  name       = var.static_rg_vnet_info["edge1"]["rg"]
 }
 
 data "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = var.rg_name
+  name                = var.static_rg_vnet_info["edge1"]["vnet"]
+  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
 }
 
 data "azurerm_subnet" "subnet" {
   name                 = var.subnet_info["edge1subnet"]["subnet_names"][count.index]
-  virtual_network_name = var.vnet_name
-  resource_group_name  = var.rg_name
+  virtual_network_name = var.static_rg_vnet_info["edge1"]["vnet"]
+  resource_group_name  = var.static_rg_vnet_info["edge1"]["rg"]
   count                = length(var.subnet_info["edge1subnet"]["subnet_names"])
 }
