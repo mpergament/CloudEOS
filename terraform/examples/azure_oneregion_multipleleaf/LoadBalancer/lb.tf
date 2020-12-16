ea340b2097b7ff157e7ce98375543b7940b8bb19
mp <mp@arista.com>
2020-12-16 19:44:12 +0100

    Extend config

--- terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
+++ terraform/examples/azure_oneregion_multipleleaf/LoadBalancer/lb.tf
@@ -24,7 +24,7 @@ data "azurerm_network_interface" "firstif" {
   resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
 }
 
-resource "azurerm_lb" "leafha_ilb" {
+resource "azurerm_lb" "cloudedgelb" {
   name                = "AMADEUS-TEST"
   location            = data.azurerm_virtual_network.vnet.location
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -33,4 +33,41 @@ resource "azurerm_lb" "leafha_ilb" {
     name      = "Amadeus-IP1"
     subnet_id = data.azurerm_subnet.subnet[0].id
   }
+  frontend_ip_configuration {
+    name      = "Amadeus-IP2"
+    subnet_id = data.azurerm_subnet.subnet[0].id
+  }
+}
+
+resource "azurerm_lb_rule" "rule1" {
+  resource_group_name            = var.static_rg_vnet_info["edge1"]["rg"]
+  loadbalancer_id                = azurerm_lb.cloudedgelb[0].id
+  name                           = "rule1"
+  protocol                       = "All"
+  frontend_port                  = 0
+  backend_port                   = 0
+  frontend_ip_configuration_name = "Amadeus-IP1"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.pool1[0].id
+  probe_id                       = azurerm_lb_probe.probe[0].id
+  load_distribution              = "Default"
+}
+
+resource "azurerm_lb_probe" "probe" {
+  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+  loadbalancer_id     = azurerm_lb.cloudedgelb[0].id
+  name                = "ssh-probe"
+  port                = 22
+  protocol            = "Tcp"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "intfpoolassociation1" {
+  network_interface_id    = azurerm_network_interface.firstif.id
+  ip_configuration_name   = "edge1cloudeos1Intf0"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.pool1[0].id
+}
+
+resource "azurerm_lb_backend_address_pool" "pool1" {
+  resource_group_name = var.static_rg_vnet_info["edge1"]["rg"]
+  loadbalancer_id     = azurerm_lb.cloudedgelb[0].id
+  name                = "CloudEOSPool1"
 }
